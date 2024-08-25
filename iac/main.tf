d9327d75b493b448169ad89fffbac4989c5a6e8a
Robyn <robyn@example.com>
2024-08-25 11:59:57 +0100

    Comment out EC2 resources

--- iac/main.tf
+++ iac/main.tf
@@ -4,28 +4,35 @@
 provider "aws" {
   region = var.region
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
-}
+#
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#
+#   filter {
+#     name = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+#
+#   filter {
+#     name = "virtualization-type"
+#     values = ["hvm"]
+#   }
+#
+#   owners = ["099720109477"] # Canonical
+# }
+#
+# resource "aws_instance" "ubuntu" {
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = var.instance_type
+#   user_data     = <<-EOF
+#               #!/bin/bash
+#               apt-get update
+#               apt-get install -y apache2
+#               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+#               echo "Hello World" > /var/www/html/index.html
+#               systemctl restart apache2
+#               EOF
+#   tags = {
+#     Name = var.instance_name
+#   }
+# }
